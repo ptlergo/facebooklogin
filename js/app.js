56792057e3b23e69069d8305565f6aa3edde7b99
ptlergo <ptlergo@gmail.com>
2016-11-14 18:46:59 -0500

    creating ui.router states

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router']);
 
-myApp.controller('appController', ['$scope', ($scope) => {
-  $scope.message = "welcome message!";
-}])
+myApp.config(['$stateProvider'], ($stateProvider) => {
+  $stateProvider
+    .state("register",{
+      url: "/register",
+      templateUrl: "views/register.html",
+      controller: "regContrller",
+      controllerAs: "regCtrl"
+    })
+    .state("login",{
+      url: "/login",
+      templateUrl: "views/login.html",
+      controller: "loginContrller",
+      controllerAs: "loginCtrl"
+    })
+});
