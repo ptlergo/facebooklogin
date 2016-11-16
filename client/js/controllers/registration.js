081f64383d9f18027e699e92068067b7f949cdf2
ptlergo <ptlergo@gmail.com>
2016-11-16 00:34:01 -0500

    firebase3 implemented. SUCCESS

--- client/js/controllers/registration.js
+++ client/js/controllers/registration.js
@@ -1,8 +1,18 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAw0vo20GbssVKhraKrkdQgwV2oALGjNDk",
+    authDomain: "facebooklogin-4a1ff.firebaseapp.com",
+    databaseURL: "https://facebooklogin-4a1ff.firebaseio.com",
+    storageBucket: "facebooklogin-4a1ff.appspot.com",
+    messagingSenderId: "998356488921"
+  };
+  firebase.initializeApp(config);
+
 myApp.controller('registrationController',
-  ['$scope', '$firebaseAuth', 'FIREBASE_URL',
-  function($scope, $firebaseAuth, FIREBASE_URL) {
-    var c = 'https://facebooklogin-4a1ff.firebaseio.com';
-    // inittialize firebase
+  ['$scope', '$firebaseAuth',
+  function($scope, $firebaseAuth) {
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth(firebase.auth());
 
     $scope.login = function() {
       $scope.message = "You're logged in under email: " + $scope.user.email;
@@ -10,6 +20,14 @@ myApp.controller('registrationController',
     };
 
     $scope.register = function() {
+      auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password
+      ).then(function(regUser) {
+        $scope.message = "Hello " + $scope.user.firstname +
+        ", thank you for registering";
 
+      }).catch(function(error) {
+        $scope.message = error.message;
+
+      });
     };
 }]);
