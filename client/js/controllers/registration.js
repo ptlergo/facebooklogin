bb269ee88e80030b1461be3b13a05a54ace982ae
ptlergo <ptlergo@gmail.com>
2016-11-18 00:10:38 -0500

    firebaseui implemented. need to connect

--- client/js/controllers/registration.js
+++ client/js/controllers/registration.js
@@ -1,18 +1,37 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAw0vo20GbssVKhraKrkdQgwV2oALGjNDk",
-    authDomain: "facebooklogin-4a1ff.firebaseapp.com",
-    databaseURL: "https://facebooklogin-4a1ff.firebaseio.com",
-    storageBucket: "facebooklogin-4a1ff.appspot.com",
-    messagingSenderId: "998356488921"
-  };
-  firebase.initializeApp(config);
-
 myApp.controller('registrationController',
   ['$scope', '$firebaseAuth',
   function($scope, $firebaseAuth) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAw0vo20GbssVKhraKrkdQgwV2oALGjNDk",
+      authDomain: "facebooklogin-4a1ff.firebaseapp.com",
+      databaseURL: "https://facebooklogin-4a1ff.firebaseio.com",
+      storageBucket: "facebooklogin-4a1ff.appspot.com",
+      messagingSenderId: "998356488921"
+    };
+
+    var uiConfig = {
+        'signInSuccessUrl': '8pounds.com',
+        'signInOptions': [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        'tosUrl': '8pounds.com/about',
+      };
+
+    var app = firebase.initializeApp(config);
+    var auth = app.auth();
+    var ui = new firebaseui.auth.AuthUI(auth);
+    ui.start('#firebaseui-auth-container', uiConfig);
+
     var ref = firebase.database().ref();
-    var auth = $firebaseAuth(firebase.auth());
+    // var auth = $firebaseAuth(firebase.auth());
+    // var provider = new firebase.auth.FacebookAuthProvider();
+
+
+
 
     $scope.login = function() {
       $scope.message = "You're logged in under email: " + $scope.user.email;
@@ -28,6 +47,12 @@ myApp.controller('registrationController',
       }).catch(function(error) {
         $scope.message = error.message;
 
+
       });
     };
+
+    $scope.fblogin = function() {
+      $scope.message = 'message!';
+
+    }
 }]);
