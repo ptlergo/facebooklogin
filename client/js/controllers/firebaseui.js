3374e2c8892356defe793125a972f7b7c3ebc43b
ptlergo <ptlergo@gmail.com>
2016-11-18 01:30:26 -0500

    firebaseui.js cleanup. each state needs to be reloaded. FIX

--- client/js/controllers/firebaseui.js
+++ client/js/controllers/firebaseui.js
@@ -19,40 +19,21 @@ myApp.controller('firebaseuiController',
         ],
         // Terms of service url.
         'tosUrl': '#/register',
-      };
+        'callbacks': {
+          'signInSuccess': function(currentUser, credential, redirectUrl) {
+            // Do something.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            console.log('success');
+            return true;
+        }
+      }
+    };
 
     var app = firebase.initializeApp(config);
     var auth = app.auth();
     var ui = new firebaseui.auth.AuthUI(auth);
     ui.start('#firebaseui-auth-container', uiConfig);
-
     var ref = firebase.database().ref();
-    // var auth = $firebaseAuth(firebase.auth());
-    // var provider = new firebase.auth.FacebookAuthProvider();
-
-
-
-
-    $scope.login = function() {
-      $scope.message = "You're logged in under email: " + $scope.user.email;
-
-    };
-
-    $scope.register = function() {
-      auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password
-      ).then(function(regUser) {
-        $scope.message = "Hello " + $scope.user.firstname +
-        ", thank you for registering";
-
-      }).catch(function(error) {
-        $scope.message = error.message;
-
-
-      });
-    };
-
-    $scope.fblogin = function() {
-      $scope.message = 'message!';
-
-    }
+    
 }]);
